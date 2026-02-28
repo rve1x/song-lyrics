385cb5d37a6de7e2dc000077d35b65f7189931ce
rve1x <hudaalemadi011@gmail.com>
2026-02-28 16:07:52 +0300

    Replace lyrics in idon'tknow.py

--- idon'tknow.py
+++ idon'tknow.py
@@ -1,27 +1,19 @@
 import time, sys 
 
 lyrics = [
-"im not baby anymore",
-"im not as innocent as before",
-"i see it the mirror. in my room",
-"and i can feel it stronger in my soul",
-"but im not so ready for this world",
-"now i see things i didn't, see before",
-"i need explannation. tell me more",
-"why i'm in love now?",
-"i don't know",
-"how can i live forever?",
-"i don't know",
-"where can i find heaven?",
-"i don't know",
-"what is going to happen?",
-"i don't know",
-"why i'm in love now?",
-"i don't know",
+"One day I'm gonna fly away",
+"One day when heaven calls my name",
+"I lay down I close my eyes at night",
+"I can see moon and light",
+"One day I'm gonna fly away",
+"One day I'll see your eyes again",
+"I lay down I close my eyes at night",
+"I can see moon and light",
 ]
 for line in lyrics:
     for ch in line+"\n":
         sys.stdout.write(ch)
         sys.stdout.flush()
         time.sleep(0.05)
+
     time.sleep(0.6)
